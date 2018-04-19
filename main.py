01b45b8b1498b237a9a73e68abc04a196a77bb55
Xia-Hu <amber.hx01@gmail.com>
2018-04-18 21:07:37 -0700

    deep chrome model

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from optparse import OptionParser
+import train_deepchrome
+import keras
+
+def training(opt):
+    # generate input data with size
+    x_train = np.random.random((opt.num_train, 1, opt.num_marks, opt.length_bin))
+    y_train = keras.utils.to_categorical(np.random.randint(
+        opt.num_label, size=(opt.num_train, 1)), num_classes=opt.num_label)
+    # train deep chrome
+    model_dchrome = train_deepchrome.train(x_train, y_train, opt)
+
+if __name__ == "__main__":
+
+    parser = OptionParser()
+    parser.add_option("--numtrain", dest="num_train", default=2000)
+    parser.add_option("--nummark", dest="num_marks", default=5)
+    parser.add_option("--lenbin", dest="length_bin", default=100)
+    parser.add_option("--nlabel", dest="num_label", default=2)
+    (options, args) = parser.parse_args()
+    
+    training(options)
---
+++ train_deepchrome.py
@@ -0,0 +1,69 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten, Dropout
+from keras import optimizers
+
+
+def train(x_train, y_train, opt):
+    ### parameter setup
+    size_batch = 1
+    num_filters = 50
+    length_filter = 10
+    length_pool = 5
+    length_full_1 = 625
+    length_full_2 = 125
+    lrate=0.01
+
+    ### construct model
+    model = Sequential()
+    # conv + relu + pooling
+    model.add(Convolution1D(
+        batch_input_shape=(size_batch, opt.num_marks, opt.length_bin),
+        filters=num_filters,
+        kernel_size = (opt.num_marks, length_filter),
+        strides=1,
+        padding='same',
+        data_format='channels_first'
+        ))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(
+        pool_size=(1, length_pool),
+        strides=length_pool,
+        padding='same',
+        data_format='chanells_first'
+        ))
+    # dropout
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    # two hidden layer
+    model.add(Dense(length_full_1))
+    model.add(Activation('relu'))
+    model.add(Dense(length_full_2))
+    model.add(Activation('relu'))
+    # output layer
+    model.add(Dense(opt.num_label))
+    model.add(Activation('softmax'))
+    
+    # loss and sgd
+    sgd = optimizers.SGD(lr=lrate)
+    model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+    ### begin training
+    model.fit(x_train, y_train, epochs = 10, batch_size = 1)
+
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
