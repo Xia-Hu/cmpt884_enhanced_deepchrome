44ef7695f946ba0d6f3b09654ee7f0b077e389cc
Young Wu <doomsplayer@gmail.com>
2018-04-22 13:57:09 -0700

    update

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import numpy as np
 import keras
+from keras.utils import *
 
 def read_data(path):
-    df = pd.read_csv(path, header=None)
-    Xs = []
-    Ys = []
+    Xs, Ys = pd.read_pickle(path)
 
-    for id, group in df.groupby(0):
-        X = group.values[None, :,2:-1]
-        Y = group.values[0, -1]
-        Xs.append(X)
-        Ys.append(Y)
+    Xtrain, Xvalid, Xtest = Xs[:,:5725], Xs[:,5725:5725*2], Xs[:,5725*2:]
+    
+    Ys = np.concatenate([to_categorical(Ys[rowid])[None,:,:] for rowid in range(len(Ys))])
+    Ytrain, Yvalid, Ytest = Ys[:,:5725], Ys[:,5725:5725*2], Ys[:,5725*2:]
 
-    Xs = np.concatenate(Xs)
-    Ys = keras.utils.to_categorical(np.asarray(Ys))
-    return (Xs, Ys)
+    return Xtrain, Xvalid, Xtest, Ytrain, Yvalid, Ytest
